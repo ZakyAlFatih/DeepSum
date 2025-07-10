69876e1aea9018a20b04a558d1cad0e7714f0029
ZakyAlFatih <111637188+ZakyAlFatih@users.noreply.github.com>
2025-07-10 21:22:52 +0700

    first commit

--- src/js/services/api_service.js
+++ src/js/services/api_service.js
@@ -2,7 +2,7 @@ import { supabase } from "../supabase_client";
 
 // const API_BASE_URL = "https://deepsum.space";
 // const API_BASE_URL = "https://51df-114-10-46-45.ngrok-free.app";
-const API_BASE_URL = "https://89c4-182-253-124-146.ngrok-free.app";
+const API_BASE_URL = " https://e9118870df4e.ngrok-free.app";
 
 async function getCurrentUser() {
   const {
